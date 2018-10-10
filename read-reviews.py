ed9a20a355318a23b447c6f1598ec93439b8ec51
Moszi <niecheeel@gmail.com>
2018-10-10 21:41:35 +0800

    add filtering

--- read-reviews.py
+++ read-reviews.py
@@ -1,3 +1,4 @@
+#Find reviews and calculate it.
 data = []
 count = 0
 with open('reviews.txt', 'r') as file:
@@ -13,3 +14,10 @@ for avg in data:
     sum_len += len(avg)
 print('Total', sum_len, 'words')
 print('Average is', sum_len/len(data), 'words')
+
+#Reviews of less than 100 words 
+new = []
+for select_avg in data:
+    if len(select_avg) < 100:
+        new.append(select_avg)
+print('Total', len(new), 'less than 100 words')
