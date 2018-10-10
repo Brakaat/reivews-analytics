13bdec47e43d17fa1e64cfc621f714f8d53b320e
Moszi <niecheeel@gmail.com>
2018-10-10 20:14:32 +0800

    cal avg

--- read-reviews.py
+++ read-reviews.py
@@ -3,6 +3,13 @@ count = 0
 with open('reviews.txt', 'r') as file:
     for line in file:
         data.append(line)
-        count += 1
-        if count % 1000 == 0:
-            print(len(data))
+        # count += 1
+        # if count % 1000 == 0:
+        #     print(len(data))
+print('Total', len(data), 'reviews')
+#print(len(data[0]))
+sum_len = 0
+for avg in data:
+    sum_len += len(avg)
+print('Total', sum_len, 'words')
+print('Average is', sum_len/len(data), 'words')
