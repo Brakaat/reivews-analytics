a8bb37e5831878d70973d70e28b3ea6c3de29fc1
Moszi <niecheeel@gmail.com>
2018-10-10 19:03:08 +0800

    add .py

---
+++ read-reviews.py
@@ -0,0 +1,8 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as file:
+    for line in file:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
